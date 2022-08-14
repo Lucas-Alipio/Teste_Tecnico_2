f63c7c461e27164372825add781ee570ed5f009a
Lucas-Alipio <lucasantonio@gec.inatel.br>
2022-08-14 17:40:30 -0300

    :recycle: refactor: Organizing the structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,44 +1,12 @@
-import 'package:bonfire/bonfire.dart';
 import 'package:flutter/material.dart';
-import 'package:teste_tecnico_2/player/mhero.dart';
+import 'package:teste_tecnico_2/starter.dart';
 
 const double tileSize = 32;
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return BonfireTiledWidget(
-      joystick: Joystick(
-        directional: JoystickDirectional(),
-      ),
-      map: TiledWorldMap(
-        'maps/map.json',
-        forceTileSize: const Size(tileSize, tileSize),
-      ),
-      player: Mhero(Vector2(tileSize, tileSize)),
-      showCollisionArea: true,
-    );
-  }
+  runApp(
+    const MaterialApp(
+      home: Starter(),
+    ),
+  );
 }
---
+++ lib/starter.dart
@@ -0,0 +1,24 @@
+import 'package:bonfire/bonfire.dart';
+import 'package:flutter/material.dart';
+
+import 'main.dart';
+
+class Starter extends StatefulWidget {
+  const Starter({Key? key}) : super(key: key);
+
+  @override
+  State<Starter> createState() => _StarterState();
+}
+
+class _StarterState extends State<Starter> {
+  @override
+  Widget build(BuildContext context) {
+    return BonfireTiledWidget(
+      map: TiledWorldMap(
+        'maps/map.json',
+        forceTileSize: const Size(tileSize, tileSize),
+      ),
+      showCollisionArea: true,
+    );
+  }
+}
